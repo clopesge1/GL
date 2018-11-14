1a5c04585134637555a6de38fe39c89a3296703c
clopes <clementl1706@gmail.com>
2018-11-14 23:04:00 +0100

    exo45 enfin fini

--- src/main.c
+++ src/main.c
@@ -1,21 +1,51 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
+#include <ansi_c.h>
 #include "my_header.h"
 #include "TP_lib.h"
 
 short int LancerDe (void);
-
+			  unsigned char score=0;
+			  short resultat=0;
+			  
 void main (void)
 {
-	
-	short resultat, i=0;
-		while(i<10)
+	  	srand(time(NULL));
+		 while(resultat!=1)
 		{
 		resultat = LancerDe();
-		i++;
+		if(resultat==4)
+		{
+			score=score+resultat;
+		}
+		
+		else if(resultat==6)
+		{
+			score=score+resultat;
+		}
+		
+		else if(resultat==2)
+		{
+			score=score+resultat;
+		}
+		
+		else if(resultat==3)
+		{
+			score = score*2;
+		}
+		
+		else if(score==5)
+		{
+			score = score-2;
+		}
+		else {}
+		
+		
+		 
 		}
 
-    i=i+0;
+          printf ("score final :%d",score);
+		  score= score+0;
 
 }
