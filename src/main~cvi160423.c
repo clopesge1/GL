2746021636b191369638f4b31f21a96ab46b35c3
clopes <clementl1706@gmail.com>
2018-11-15 16:04:59 +0100

    modification légère

---
+++ src/main~cvi160423.c
@@ -0,0 +1,51 @@
+//  Copyright (c) 2018 Antoine Tran Tan
+//
+
+#include <ansi_c.h>
+#include "my_header.h"
+#include "TP_lib.h"
+
+
+			  unsigned char score=0;
+			  short resultat=0;
+			  
+void main (void)
+{
+	  	srand(time(NULL));
+		 while(resultat!=1)
+		{
+		resultat = LancerDe();
+		if(resultat==4)
+		{
+			score=score+resultat;
+		}
+		
+		else if(resultat==6)
+		{
+			score=score+resultat;
+		}
+		
+		else if(resultat==2)
+		{
+			score=score+resultat;
+		}
+		
+		else if(resultat==3)
+		{
+			score = score*2;
+		}
+		
+		else if(score==5)
+		{
+			score = score-2;
+		}
+		else {}
+		
+		
+		 
+		}
+
+          printf ("score final :%d",score);
+		  score= score+0;
+
+}
