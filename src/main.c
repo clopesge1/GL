90e0bc009900a443d0c2c76e20797a1d795432fd
clement lopes <clement.lopes@ge1.iut-cachan.u-psud.fr>
2018-11-20 11:50:02 +0100

    petit2

--- src/main.c
+++ src/main.c
@@ -7,11 +7,13 @@
 #define seuilh 200
 #define seuilb -100
 void bin_tab(void); 
+void sat_tab(void);
 unsigned char bin[10]={0,1,2,3,4,5,6,7,8,9};
 unsigned char sat[10]={0,1,2,3,4,5,6,7,8,9};
 int i;
 int main(void)
 {
+	sat_tab();
 	bin_tab();
 	
     return 0;
@@ -33,4 +35,19 @@ int main(void)
 	}
 }
 
-	
+	void sat_tab(void)
+	{
+		char val=0;
+		for (i=0 ; i>9; i++);
+		val = (rand()%400+1)-100;
+	if (val > seuilh)
+		{ sat[i] = seuilh;
+		}
+		
+				 else if (val < seuilb) 
+		{ sat[i] = seuilb;
+		}
+			 else  {
+				 sat[i] = val;
+		}
+	}
