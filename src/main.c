b9f7484f7fd5ea20d9f75e8c2153a414a9e22a87
clopes <clementl1706@gmail.com>
2018-11-20 22:30:48 +0100

    parti 5 exo 56 enfin fini (je peut dormir maintenant)

--- src/main.c
+++ src/main.c
@@ -7,17 +7,21 @@
 #define seuil 5
 #define seuilh 200
 #define seuilb -100
+#define N 10
 void bin_tab(void); 
 void sat_tab(void);
 void dist_tab(void);
 void dif2_tab(void);
+void miroir_tab(void); 
 unsigned char bin[10]={0,1,2,3,4,5,6,7,8,9};
 unsigned char sat[10]={0,1,2,3,4,5,6,7,8,9};
 unsigned char dist[10]={0,1,2,3,4,5,6,7,8,9};
 unsigned char A[7]={0,7,19,3,8,15,11};
 unsigned char B[6]={0,1,2,3,4,5,6}; 
+unsigned char miroir[N]={0,1,2,3,4,5,6,7,8,9};
 int i;
 int n;
+int inv;
 int main(void)
 {
 	srand(time(NULL));							 
@@ -25,6 +29,7 @@ int main(void)
 	bin_tab();
 	dist_tab();
 	dif2_tab();
+	miroir_tab();
 	
     return 0;
 }
@@ -73,6 +78,21 @@ void dif2_tab(void)
 for ( i=6, n=0 ; i>0 ; i--, n++)
     {
 	  B[n] = A[i];
+    }
+	
+}
+
+void miroir_tab(void) 
+{
+ inv=N/2;
+ 
+ 	for ( i=0, n=9; i<10 ; i++,n--)
+    {
+	  if (i>=inv)
+	  {
+	  miroir[i] = miroir[n];
+	  }
+				  miroir[i];
     }
 	
 }
