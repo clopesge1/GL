e0eded61e515143822ac139f5e315cbe54ca8739
clement lopes <clement.lopes@ge1.iut-cachan.u-psud.fr>
2018-11-27 11:45:43 +0100

    a finir a la maison

--- src/main.c
+++ src/main.c
@@ -2,8 +2,35 @@
 //
 
 #include "my_header.h"
+#include "TP_lib_suite.h"
+		unsigned char lotowin[6]={11,34,7,12,31,5};
+		unsigned char lotojoueur[6]={5,34,27,2,31,3};
+void verif (unsigned char lotowin[6],unsigned char lotojoueur[6]);
+
+   int i,n;
+   int val_egal=0;
+
 
 int main(void)
 {
+
+verif(lotowin,lotojoueur);
+	
     return 0;
+}
+
+
+void verif ( unsigned char lotowin[6],unsigned char lotojoueur[6])
+{
+  for (i=0 ; i<6; i++){
+	  
+  for(n=0; n<6; n++){
+		  if(lotowin[i]==lotojoueur[n])
+		  {
+		    val_egal = val_egal + 1;
+		  }
+  }
+	  
+	  
+  }
 }
