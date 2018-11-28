5c932ef46ecea7d5c97c9b1f5664072a91e36a7a
clopes <clementl1706@gmail.com>
2018-11-28 20:57:24 +0100

    exo terminer

--- src/main.c
+++ src/main.c
@@ -12,7 +12,8 @@ void verif (unsigned char lotowin[6],unsigned char lotojoueur[6]);
 void verif_random (unsigned char lotowinrandom[6],unsigned char lotojoueur[6]);
 void verif_random100 (unsigned char lotowinrandom[6],unsigned char lotojoueur[6]);
 
-   unsigned char rendu=0;
+   unsigned char tirage=0;
+   unsigned char memoire=0;
    unsigned char i,b,r;
    int val_egal=0;
    int val_egal1=0;
@@ -24,7 +25,7 @@ int main(void)
 
 verif(lotowin,lotojoueur);
 verif_random(lotowinrandom,lotojoueur);
-//verif_random100(lotowinrandom,lotojoueur);
+verif_random100(lotowinrandom,lotojoueur);
 	
     return 0;
 }
@@ -34,13 +35,19 @@ void verif_random100 (unsigned char lotowinrandom[6],unsigned char lotojoueur[6]
 {
 	initialiserTirage();
    for (i=0 ; i<100; i++){
+	   
+	     if (val_egal100 > memoire)
+		 {memoire = val_egal100;
+		 }
+	     else {
+	     val_egal100=0;
+		 }
    		for (r=0 ; r<6; r++){
-			lotojoueur[r];
-			
+			tirage =  tirerNumero ();	   // appel de fonction pas toucher NB 
+			lotowinrandom[r]= tirage ;
 	   		for (b=0 ; b<6; b++){
-			rendu =  tirerNumero ();			 // appel de fonction pas toucher NB 
-	    	lotowinrandom[b] = rendu ;
-				if(lotowinrandom[b]==lotojoueur[r])
+				lotojoueur[b];
+				if(lotowinrandom[r]==lotojoueur[b])
 		   	{
 		    val_egal100 = val_egal100 + 1;
 		    	}
@@ -52,11 +59,11 @@ void verif_random100 (unsigned char lotowinrandom[6],unsigned char lotojoueur[6]
 void verif_random (unsigned char lotowinrandom[6],unsigned char lotojoueur[6])
 {
 	initialiserTirage(); 
-for (i=0 ; i<6; i++){	
-		lotojoueur[i];
+for (i=0 ; i<6; i++){
+	tirage =  tirerNumero ();			 // appel de fonction pas toucher NB 
+	lotowinrandom[i] = tirage ;
 	 for(b=0; b<6; b++){
-	rendu =  tirerNumero ();			 // appel de fonction pas toucher NB 
-	lotowinrandom[b] = rendu ;
+		lotojoueur[b]; 
 		if(lotowinrandom[b]==lotojoueur[i]) {
 		 val_egal1 = val_egal1 + 1; 	
 	}
