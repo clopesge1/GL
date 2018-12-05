09273c26f3f652d1750c61569b601b80d7a726a3
clopes <clementl1706@gmail.com>
2018-12-05 20:11:05 +0100

    exo_79 fini

--- src/main.c
+++ src/main.c
@@ -7,21 +7,22 @@
 int main(void)
 {  
 char phrase[99] = "je mappelle Americo Lopes et j'ai 19 ans";
- int age;
+unsigned int age;
 char prenom [99];
 char nom [99]; 
 
 
-	//sscanf(phrase,"%*s%*s%s%s%*s%*s%d%*s",prenom,&nom,&age);
-	printf("prenom");
+	printf("inscrire le prenom");
 	scanf("%s",prenom );
 	
-	printf("nom");
+	printf("inscrire le nom");
 	scanf("%s",nom );
 	
 	printf("age");
 	scanf("%d",&age );
 	
+	
+	sprintf(phrase,"je mappelle %s %s et j'ai %d ans", prenom,nom,age);
 	sscanf(phrase,"%*s%*s%s%s%*s%*s%d%*s",prenom,nom,&age);
 	return 0; 
 }
