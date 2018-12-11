579176a1a72a61f9944a1c9bfc6e5efa3d459c0b
clement lopes <clement.lopes@ge1.iut-cachan.u-psud.fr>
2018-12-11 11:33:10 +0100

    25

--- src/main.c
+++ src/main.c
@@ -6,22 +6,6 @@
 
 int main(void)
 {  
-char phrase[99] = "je mappelle Americo Lopes et j'ai 19 ans";
- int age;
-char prenom [99];
-char nom [99]; 
 
-
-	//sscanf(phrase,"%*s%*s%s%s%*s%*s%d%*s",prenom,&nom,&age);
-	printf("prenom");
-	scanf("%s",prenom );
-	
-	printf("nom");
-	scanf("%s",nom );
-	
-	printf("age");
-	scanf("%d",&age );
-	
-	sscanf(phrase,"%*s%*s%s%s%*s%*s%d%*s",prenom,nom,&age);
-	return 0; 
+	return 0;
 }
