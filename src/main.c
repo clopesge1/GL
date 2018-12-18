b1fdfda5861bbac162b31aade927b528c9e69453
clement lopes <clement.lopes@ge1.iut-cachan.u-psud.fr>
2018-12-18 11:21:13 +0100

    25

--- src/main.c
+++ src/main.c
@@ -1,11 +1,27 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
-#include <ansi_c.h>
-#include "my_header.h"
+#include <iostream>
+#include <my_header.hpp>
+#include <cmath>
+#include <cstdint>
 
-int main(void)
-{  
+int main()
+{
+    long int anneau1, anneau2, anneau3, val_res;
+    int16_t R;
 
-	return 0;
-}
+    anneau1 = saisir_anneau(0,9,1);
+
+    anneau2 = saisir_anneau(0,9,2);
+
+    anneau3 = saisir_anneau(0,6,3);
+
+
+    // if (c > 7)
+    //     R = -1;
+    // else
+    //     R = (10 * a + b) * (uint16_t)pow(10,c);
+
+
+    return 0;
